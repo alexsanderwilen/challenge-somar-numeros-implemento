e01b5002b07f23242f9c46a6ce6323e51f06fb67
Alexsander Wilen <alexsanderwilen@sancode.com.br>
2025-07-02 22:05:43 -0300

    Update somador.go

--- internal/somador/somador.go
+++ internal/somador/somador.go
@@ -2,7 +2,10 @@ package somador
 
 // SomarNumeros recebe um slice de inteiros e retorna a soma deles.
 func SomarNumeros(nums []int) int {
-	// TODO: implementar
-	return 0
+	soma := 0
+	for _, num := range nums {
+		soma += num
+	}
+	return soma
 }
 
